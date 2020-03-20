17f246aa6d5622738b44d0054d9f63fa31892749
zigenstern <61959179+zigenstern@users.noreply.github.com>
2020-03-20 01:54:21 -0400

    biggay

---
+++ Assets/Scripts/gunsound.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class gunsound : MonoBehaviour
+{
+	public AudioClip GunSound;
+ void Start()
+    {GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = GunSound;
+        
+    }
+    void Update()
+    {
+		if(Input.GetKeyDown("Fire1"))
+		GetComponent<AudioSource>().Play();
+        
+    }
+}
